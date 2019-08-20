f44950393256d0625b742ff562a878c1293a19bb
Bianca Gotaski <biancagotaski@outlook.com.br>
2019-08-20 18:42:39 -0300

    controllers created

---
+++ src/main/java/com/mycompany/aulaspring2/controller/CustomerController.java
@@ -0,0 +1,20 @@
+package com.mycompany.aulaspring2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Bianca
+ */
+@Controller
+public class CustomerController {
+    
+    @RequestMapping("/url")
+    public String page(Model model) {
+        model.addAttribute("attribute", "value");
+        return "view.name";
+    }
+    
+}
